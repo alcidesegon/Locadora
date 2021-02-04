a52b8d0bf76ebd80199ef66e9cca15f353aa6c49
Master <alcides_egon@hotmail.com>
2021-02-04 10:09:24 -0300

    master - Inclusão de validação de DBConnection

--- Login/Conexao.cs
+++ Login/Conexao.cs
@@ -18,6 +18,10 @@ namespace Login
         public static SqlConnection GetConexao()
         {
             string dbConnection = ConfigurationSettings.AppSettings["DBConnection"];
+            if (string.IsNullOrEmpty(dbConnection))
+            {
+                throw new Exception("Configuração de banco de dados não encontrada!");
+            }
 
             SqlConnection conexao = new SqlConnection(dbConnection);
 
